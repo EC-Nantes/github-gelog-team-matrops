184d99b380ced1682d5ae2ff89f7d81d2503aba3
Max <Max@ext-130-66-208-140-eduroam.ec-nantes.fr>
2025-09-19 09:38:50 +0200

    TEST

---
+++ TeamMatrops/src/main/java/org/centrale/projet/objet/teammatrops/TeamMatrops.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.projet.objet.teammatrops;
+
+/**
+ *
+ * @author Max
+ */
+public class TeamMatrops {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
